7222520cd81c760c0411124689b366c1c7e8ce87
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-01-26 19:26:29 -0500

    added icon for linkedin and github and updated background image

--- react-portfolio/src/componets/Main.jsx
+++ react-portfolio/src/componets/Main.jsx
@@ -1,7 +1,8 @@
-// Here we have imported react, viteLogo and TypeAnimation from react-type-animation. We downloaded the react-type-animation from npmjs.com and followed the installation instructions.We copied the <TypeAnimation /> component from usage part of the page to make the text move as seen below. Make sure to npm run dev again once installing a new package.
+// Here we have imported react, viteLogo and TypeAnimation from react-type-animation. We downloaded the react-type-animation from npmjs.com and followed the installation instructions.We copied the <TypeAnimation /> component from usage part of the page to make the text move as seen below. Make sure to npm run dev again once installing a new package.I also added the FaGithub and FaLinkedin icons from react-icons.
 import React from "react";
 import viteLogo from "/vite.svg";
 import { TypeAnimation } from "react-type-animation";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -9,7 +10,7 @@ const Main = () => {
       {/* For the image we have it applied to the width of the full screen, as well as the height of the screen, we also set the object-cover so that the image is not stretched */}
       <img
         className="w-full h-screen object-cover"
-        src="./src/componets/images/mountains.jpeg"
+        src="./src/componets/images/forest.jpeg"
       />
       {/* For the text, it is set as absolute so that it can be positioned on the image as well set it to top 1/2 of the screen which moved it halfway up the screen. The settings are placed in the DIV so it is applied to all the children */}
       <div className="absolute top-1/2 w-full text-center text-white text-3xl>">
@@ -33,6 +34,11 @@ const Main = () => {
             repeat={Infinity}
           />
         </h2>
+        {/* Here we have applied the FaGithub and FaLinkedin icons, they are set to absolute so that they can be positioned on the image */}
+        <div className="absolute flex justify-center w-full p-4 space-x-4">
+          <FaLinkedin className="cursor-pointer" size ={20}/>
+          <FaGithub className="cursor-pointer" size ={20}/>
+        </div>
       </div>
     </div>
   );
