8b1d18136422a95f829b3a64beebdf1d9b46fd2a
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-01-26 19:11:25 -0500

    added comments to the app.jsx file

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+// In the app.jsx file we have imported useState, viteLogo and Navbar from componets as well as Main from componets. We update our app.jsx as we add componets so that the componeets we are adding are displayed on the screen
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import Navbar from './componets/Navbar'
@@ -5,7 +6,7 @@ import Main from './componets/Main'
 
 function App() {
   
-
+// Here we add each compnet we create to make sure they are displayed on the screen
   return (
     <div>
       <Navbar />
